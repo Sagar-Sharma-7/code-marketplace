60ccfb31c9521879b6bbfda634c488770e07ad06
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2023-11-26 15:43:20 +0530

    reveal animation

--- src/main.js
+++ src/main.js
@@ -59,3 +59,20 @@ cancelBtn.addEventListener("click", () => {
 uploadBtn.addEventListener("click", () => {
   uploadWrap.style.display = "none";
 });
+
+// reveal
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 50;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    }
+  }
+}
